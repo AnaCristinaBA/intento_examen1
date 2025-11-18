7a521a002cbab2b985b49529fa97b1ec55e0ccc9
AnaCristinaBA <anacbagudo@gmail.com>
2025-11-18 09:57:45 +0100

    middelware requireAdmin no funciona

--- api/src/app.js
+++ api/src/app.js
@@ -6,7 +6,28 @@ const port = 3000
 
 app.use(express.json());
 
-app.get('/api/users', async (req, res) => {
+const requireAdmin = async function miMidelware(req, res, next) {
+  if (req.body) {
+    const { name, password } = req.body; //creo las constantes para que sepa cual es el objeto json, para asignarle valor
+
+    const [results, fields] = await connection.query( // ponemos la promesa --> async-await
+      'SELECT role FROM `users` WHERE name = ? and password = ?'
+      [name, password]
+    );
+    const role = results[0].role;
+    if (role == 'admin') {
+      next()
+    }
+    else {
+      res.status(403).send('Forbidden')
+
+    }
+  } else {
+    res.status(401).send('Not authorised');
+  }
+}
+
+app.get('/api/users', requireAdmin, async (req, res) => {
   // A simple SELECT query
   try {
     const [results, fields] = await connection.query( // ponemos la promesa --> async-await
@@ -23,10 +44,10 @@ app.post('/api/users', async (req, res) => {
   try {
     if (req.body) {
       const { name, password, role } = req.body; //creo las constantes para que sepa cual es el objeto json , para asignarle valor
-      
+
       if (!name || !password || !role) {
-      return res.status(400).send("Faltan campos obligatorios: name, password, role");
-    }
+        return res.status(400).send("Faltan campos obligatorios: name, password, role");
+      }
 
       const [results, fields] = await connection.query( // ponemos la promesa --> async-wait
         'INSERT INTO `personas` (name, password, role) VALUES (?,?,?)',
